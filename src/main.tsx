bbee184c6ff3f5340ae746251eb086051efca0ba
AlexSkilled <olegthander@yandex.ru>
2024-01-04 21:53:07 +0300

    [init]

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import WebApp from '@twa-dev/sdk'
+
+import App from './App.tsx'
+
+WebApp.ready();
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+})
